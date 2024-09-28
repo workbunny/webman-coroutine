a863c73d1e5d45f144de36666255f12e16df0d71
chaz6chez <chaz6chez1993@outlook.com>
2024-09-28 11:38:28 +0800

    feat:新增异常类

---
+++ src/Exceptions/RuntimeException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author workbunny/Chaz6chez
+ * @email chaz6chez1993@outlook.com
+ */
+declare(strict_types=1);
+
+namespace Workbunny\WebmanCoroutine\Exceptions;
+
+class RuntimeException extends \RuntimeException
+{
+}
